28e151fdb94cbc4bcaa9c84a6165e3d21cb1b307
Rafal41 <124187241+Rafal41@users.noreply.github.com>
2023-02-01 17:54:45 +0100

    Add files via upload

---
+++ R8Z6.cpp
@@ -0,0 +1,68 @@
+/*Jak sobie odkomentujesz to będzie można wykorzystać szablon dla int i double*/
+#include <iostream>
+#include <cstring>
+// #define SIZEINT 5
+// #define SIZEDOUBLE 4
+#define SIZECHAR 4
+#define SIZESTR 40
+
+template <typename T>                               //szablon 
+T maxn(T * n, const int m);
+
+template <> 
+char * maxn<char *>(char **tab, const int size);
+
+int main(void)
+{
+    using namespace std;
+    // int tab[SIZEINT];
+    // double tabdouble[SIZEDOUBLE];
+    char * tabchar[SIZECHAR];
+
+    // cout << "Podaj 5 wartosci typu int: ";
+    // for (int i = 0; i < SIZEINT; i++)
+    //     cin >> tab[i];
+    // cout << "Wartosc maksymalna ze zbioru to: " << maxn(tab,SIZEINT) << endl;
+
+    // cout << "Podaj 4 wartosci typu double: ";
+    // for (int i = 0; i < SIZEDOUBLE; i++)
+    //     cin >> tabdouble[i];
+    // cout << "Wartosc maksymalna ze zbioru to: " << maxn(tabdouble, SIZEDOUBLE) << endl;
+    // getchar();
+    
+    cout << "Podaj 4 lancuchy: ";
+    for (int i = 0; i < SIZECHAR; i++)
+    {
+        tabchar[i] = new char[SIZESTR];
+        cin.getline(tabchar[i],SIZESTR);
+    }
+        
+    // for (int i = 0; i < SIZECHAR; i++)
+    //     cout << tabchar[i]<< endl;
+   
+    cout << "Najdluzszy lancuch ze zbioru to: " << maxn(tabchar, SIZECHAR) << endl;
+
+    for (int i = 0; i < SIZECHAR; i++)
+        delete tabchar[i];
+    return 0;
+}
+
+template <typename T>
+T maxn(T * n, const int m)
+{
+    // T max = n[0];
+    // for (int i = 1; i < m; i++)
+    //     max = n[i] > max? n[i]: max;
+    // return max;
+}
+
+template <>  char * maxn<char *>(char **tab, const int size)
+{
+    char * max = tab[0];
+    for (int i = 0; i < size; i++)
+        max = strlen(tab[i]) > strlen(max) ? tab[i]: max;
+    
+    return max;
+}
+
+
